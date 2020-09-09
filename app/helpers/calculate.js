bc6aa0b02ea40fbec7473d6a0580ec14fb790bb1
Stéphane Meny <smeny@stephanes-mbp.home>
2020-09-09 14:08:16 +0100

    Extracted test

---
+++ app/helpers/calculate.js
@@ -0,0 +1,6 @@
+function calculate(cost, bags) {
+	const calculation = Number(cost) * Number(bags);
+	return calculation;
+}
+
+export default calculate
---
+++ tests/unit/corn.test.js
@@ -0,0 +1,9 @@
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+test('cost of four bags should be 1', () => {
+
+    expect(1).toBe(1);
+});
